266632ed7a0b938af91f8ce1e50d30c0f7ad0aaa
Juan Manuel Valencia Zapata <jmvalenciz@protonmail.com>
2023-09-14 22:38:29 -0500

    Add space to run pipeline

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -4,7 +4,7 @@ package co.edu.eafit.protobootapp;
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
-        return numero * numero + 1;
+        return numero * numero +  1;
     }
     
     public Double cubo(Double numero) {
